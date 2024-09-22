f0c1b29faabd06809a564336d14bd2ddbfbddc27
Deepanshu2501 <tomarvashu445@gmail.com>
2024-09-22 20:10:58 +0530

    connect mongoDb

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// require('dotenv').config({path:'./env'}) 
+import dotenv from "dotenv"
+import connectDB from "./db/db.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()   
+
+
+
+
+
+
+
+
+
+
+
+
+/*  Approach 1:making and exporting function in same file
+
+import mongoose from "mongoose";
+import {DB_NAME} from './constants'
+import express from "express"
+const app = express()
+
+// if-es method
+( async () => {
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("Error", (error) => {
+            console.error("Error: ", error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR:",error)
+        throw error
+    }
+})()
+
+*/
