5a51177478636b91580c21c2af44c73190c045eb
Deepanshu2501 <tomarvashu445@gmail.com>
2024-09-23 20:12:07 +0530

    utils

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// for config of json and url type request/form
+app.use(express.json({limit: "15kb"}))
+app.use(express.urlencoded({extended: true, limit: "15kb"}))
+
+// for storing public assets like images and fevicon
+app.use(express.static("public"))
+app.use(cookieParser()) 
+
+export { app } 
