32ac4211a9108f3648b22dc39b05bd6418459731
Deepanshu2501 <tomarvashu445@gmail.com>
2024-09-27 14:17:36 +0530

    all setup done

--- src/app.js
+++ src/app.js
@@ -17,4 +17,17 @@ app.use(express.urlencoded({extended: true, limit: "15kb"}))
 app.use(express.static("public"))
 app.use(cookieParser()) 
 
+
+// router import
+import userRouter from "./routes/user.routes.js"
+
+
+// router declaration
+app.use("/api/v1/users", userRouter)
+// http://localhost:8000/api/v1/users/register
+
 export { app } 
+
+
+// here it is necessary to use middleware to access router as we have write all functioning separately.
+// So we used app.use() instead of app.get() done previously
